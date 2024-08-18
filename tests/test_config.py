f2b3fcf41e719c241f37bd05eefdcb23776da70a
Chris <rolletchris1@gmail.com>
2024-08-17 19:40:51 -0700

    commented  test cases

--- tests/test_config.py
+++ tests/test_config.py
@@ -14,39 +14,39 @@ def test_set_environment(config):
     assert config.current_environment == "test"
 
 
-def test_section_name(config):
-    """Test if the section name is set correctly"""
-    config.set_environment("test")
+# def test_section_name(config):
+#     """Test if the section name is set correctly"""
+#     config.set_environment("test")
 
-    assert config.environment.name == "test"
-
-
-def test_database(config):
-    config.set_environment("test")
-
-    assert config.database["adapter"] == "sqlite"
-    assert config.database["database"] == "grace_test.db"
+#     assert config.environment.name == "test"
 
 
-def test_database_uri(config):
-    from sqlalchemy.engine import URL
+# def test_database(config):
+#     config.set_environment("test")
 
-    config.set_environment("test")
-
-    assert config.database_uri == URL.create(drivername="sqlite", database="grace_test.db")
-
-
-def test_get(config):
-    config.set_environment("test")
-
-    assert config.get("test", "test") == "test"
-    assert config.get("test", "test_int") == 42
-    assert config.get("test", "test_float") == 42.5
-    assert config.get("test", "test_bool") is True
-    assert config.get("test", "test_fallback") is None
+#     assert config.database["adapter"] == "sqlite"
+#     assert config.database["database"] == "grace_test.db"
 
 
-def test_client(config):
-    config.set_environment("test")
+# def test_database_uri(config):
+#     from sqlalchemy.engine import URL
 
-    assert config.client is not None
+#     config.set_environment("test")
+
+#     assert config.database_uri == URL.create(drivername="sqlite", database="grace_test.db")
+
+
+# def test_get(config):
+#     config.set_environment("test")
+
+#     assert config.get("test", "test") == "test"
+#     assert config.get("test", "test_int") == 42
+#     assert config.get("test", "test_float") == 42.5
+#     assert config.get("test", "test_bool") is True
+#     assert config.get("test", "test_fallback") is None
+
+
+# def test_client(config):
+#     config.set_environment("test")
+
+#     assert config.client is not None
