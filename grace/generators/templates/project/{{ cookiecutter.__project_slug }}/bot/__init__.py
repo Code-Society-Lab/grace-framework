3861957c535129b880ff0575f467952c0ae4abfd
penguinboi <simon.roy1211@gmail.com>
2025-02-28 02:33:49 -0500

    Removed old run function

--- grace/generators/templates/project/{{ cookiecutter.__project_slug }}/bot/__init__.py
+++ grace/generators/templates/project/{{ cookiecutter.__project_slug }}/bot/__init__.py
@@ -2,7 +2,4 @@ from grace.application import Application
 from bot.{{ cookiecutter.__project_slug }} import {{ cookiecutter.__project_class }}
 
 app = Application()
-
-
-def run():
-    {{ cookiecutter.__project_class }}(app).run()
+bot = {{ cookiecutter.__project_class }}(app)
