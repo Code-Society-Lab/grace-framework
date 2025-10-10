d4b89e34a3f658df67f8a051de9662325834fce4
penguinboi <simon.roy1211@gmail.com>
2025-10-10 03:13:33 -0400

    Cleaned up query system and new model

--- grace/model.py
+++ grace/model.py
@@ -1,114 +1,117 @@
 from sqlmodel import *
-from sqlalchemy import Engine
-from typing import TypeVar, Type, List, Optional, Any
+from typing import TYPE_CHECKING, TypeVar, Type, List, Optional, Self, Any, Union
+from sqlmodel.main import SQLModelMetaclass
 
-# Type variable for proper type hints
-T = TypeVar('T', bound='Model')
+if TYPE_CHECKING:
+    from sqlmodel.sql._expression_select_gen import Select, SelectOfScalar
+    from sqlmodel import SQLModel, Session, select, func
 
-class _ModelMeta(type(SQLModel)):
+
+T = TypeVar("T", bound="Model")
+
+
+class _ModelMeta(SQLModelMetaclass):
     """Metaclass to make table=True the default"""
 
     def __new__(cls, name, bases, namespace, **kwargs):
-        if name != 'Model':
-            if 'table' not in kwargs:
-                kwargs['table'] = True
+        if name != "Model":
+            if "table" not in kwargs:
+                kwargs["table"] = True
         return super().__new__(cls, name, bases, namespace, **kwargs)
 
 
-class Model(SQLModel, metaclass=_ModelMeta):
-    _engine: Engine = None
-
-    @classmethod
-    def set_engine(cls, engine: Engine):
-        """Set the database engine for all models"""
-        cls._engine = engine
-
-    @classmethod
-    def create(cls: Type[T], **kwargs) -> T:
-        """Create and save a new record"""
-        instance = cls(**kwargs)
-        return instance.save()
-
-    @classmethod
-    def where(cls: Type[T], *conditions) -> 'QueryBuilder[T]':
-        """Start a query with WHERE conditions"""
-        return QueryBuilder(cls).where(*conditions)
-
-    @classmethod
-    def all(cls: Type[T]) -> List[T]:
-        """Get all records"""
-        return QueryBuilder(cls).all()
-
-    @classmethod
-    def first(cls: Type[T]) -> Optional[T]:
-        """Get first record"""
-        return QueryBuilder(cls).first()
-
-    @classmethod
-    def find(cls: Type[T], id: Any) -> Optional[T]:
-        """Find by primary key"""
-        with Session(cls._engine) as session:
-            return session.get(cls, id)
-
-    def save(self: T) -> T:
-        """Save the current instance to database"""
-        with Session(self._engine) as session:
-            session.add(self)
-            session.commit()
-            session.refresh(self)
-            return self
-
-    def delete(self) -> None:
-        """Delete the current instance from database"""
-        with Session(self._engine) as session:
-            # Get the instance from the session
-            instance = session.get(self.__class__, self.id)
-            if instance:
-                session.delete(instance)
-                session.commit()
-
-    def update(self, **kwargs) -> 'Model':
-        """Update instance attributes and save"""
-        for key, value in kwargs.items():
-            if hasattr(self, key):
-                setattr(self, key, value)
-        return self.save()
-
-
-class QueryBuilder:
+class Query:
     def __init__(self, model_class: Type[T]):
         self.model_class = model_class
-        self.statement = select(model_class)
+        self.session: Session = model_class.get_session()
+        self.statement: Union[Select, SelectOfScalar] = select(model_class)
 
-    def where(self, *conditions) -> 'QueryBuilder[T]':
+    def where(self, *conditions) -> Self:
         for condition in conditions:
             self.statement = self.statement.where(condition)
         return self
 
-    def limit(self, count: int) -> 'QueryBuilder[T]':
+    def limit(self, count: int) -> Self:
         self.statement = self.statement.limit(count)
         return self
 
-    def offset(self, count: int) -> 'QueryBuilder[T]':
+    def offset(self, count: int) -> Self:
         self.statement = self.statement.offset(count)
         return self
 
-    def order_by(self, *columns) -> 'QueryBuilder[T]':
+    def order_by(self, *columns) -> Self:
         self.statement = self.statement.order_by(*columns)
         return self
 
     def all(self) -> List[T]:
-        """Execute query and return all results"""
-        with Session(self.model_class._engine) as session:
-            return list(session.exec(self.statement).all())
+        return list(self.session.exec(self.statement).all())
 
     def first(self) -> Optional[T]:
-        """Execute query and return first result"""
-        with Session(self.model_class._engine) as session:
-            return session.exec(self.statement).first()
+        return self.session.exec(self.statement).first()
 
-    def one(self) -> T:
-        """Execute query and return exactly one result"""
-        with Session(self.model_class._engine) as session:
-            return session.exec(self.statement).one()
+    def one(self) -> Type[T]:
+        return self.session.exec(self.statement).one()
 
+    def count(self) -> int:
+        count_statement: SelectOfScalar[int] = select(func.count()).select_from(
+            self.statement.subquery()
+        )
+        return self.session.exec(count_statement).one()
+
+
+class Model(SQLModel, metaclass=_ModelMeta):
+    _session: Session | None = None
+
+    @classmethod
+    def set_session(cls, session: Session):
+        cls._session = session
+
+    @classmethod
+    def get_session(cls) -> Session:
+        """Get the current session"""
+        if cls._session is None:
+            raise RuntimeError(
+                f"No session set for {cls.__name__}. Call Model.set_session() first."
+            )
+        return cls._session
+
+    @classmethod
+    def create(cls: Type[T], **kwargs) -> T:
+        instance = cls(**kwargs)
+        return instance.save()
+
+    @classmethod
+    def where(cls: Type[T], *conditions) -> Query:
+        return Query(cls).where(*conditions)
+
+    @classmethod
+    def all(cls: Type[T]) -> List[T]:
+        return Query(cls).all()
+
+    @classmethod
+    def first(cls: Type[T]) -> Optional[T]:
+        return Query(cls).first()
+
+    @classmethod
+    def find(cls: Type[T], id_: Any) -> Optional[T]:
+        return cls.get_session().get(cls, id_)
+
+    @classmethod
+    def count(cls: Type[T]) -> int:
+        return Query(cls).count()
+
+    def save(self: T) -> T:
+        self.get_session().add(self)
+        self.get_session().commit()
+        self.get_session().refresh(self)
+        return self
+
+    def delete(self) -> None:
+        self.get_session().delete(self)
+        self.get_session().commit()
+
+    def update(self, **kwargs) -> Self:
+        for key, value in kwargs.items():
+            if hasattr(self, key):
+                setattr(self, key, value)
+        return self.save()
