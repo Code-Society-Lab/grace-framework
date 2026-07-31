06ed1cc9ca2947d0151ba2ddaf4e16af6215f397
Simon Roy <49426163+PenguinBoi12@users.noreply.github.com>
2026-07-31 01:03:12 -0400

    Dyanmic versionning (#16)

--- grace/__init__.py
+++ grace/__init__.py
@@ -1,3 +1,8 @@
-__version__ = "1.0.5-alpha"
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("grace-framework")
+except PackageNotFoundError:
+    from grace._version import version as __version__
 
 from discord.ext.commands import *
